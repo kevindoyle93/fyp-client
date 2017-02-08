9ed3b1e9f9d540344f53eae9ac779e17c92c7f2b
Kevin Doyle <C13547287@mydit.ie>
2017-02-08 14:15:05 +0000

    Display sample matches in list on matches page

--- src/pages/matches/matches.ts
+++ src/pages/matches/matches.ts
@@ -8,8 +8,40 @@ import { NavController } from 'ionic-angular';
 })
 export class MatchesPage {
 
-  constructor(public navCtrl: NavController) {
+  matches: Array<any>;
 
+  constructor(public navCtrl: NavController) {
+    this.fetchMatches();
+  }
+
+  private fetchMatches = () => {
+    this.matches = [
+      {
+        homeTeam: 'Man United',
+        awayTeam: 'Man City',
+        fullTimeHomeGoals: 2,
+        fullTimeAwayGoals: 2,
+        date: '24/02/2017'
+      },
+      {
+        homeTeam: 'Chelsea',
+        awayTeam: 'Man United',
+        fullTimeHomeGoals: 1,
+        fullTimeAwayGoals: 0,
+        date: '17/02/2017'
+      },
+      {
+        homeTeam: 'West Ham',
+        awayTeam: 'Man United',
+        fullTimeHomeGoals: 0,
+        fullTimeAwayGoals: 2,
+        date: '14/02/2017'
+      }
+    ]
+  };
+
+  private onMatchClicked = (match: any) => {
+    console.log(match);
   }
 
 }
