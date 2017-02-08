e88cd72efaa0fe7d2f13928074495f97d9bcea19
Kevin Doyle <C13547287@mydit.ie>
2017-02-08 18:17:32 +0000

    Add tactical advice page with list of cards of samples

--- src/pages/tactics/tactics.ts
+++ src/pages/tactics/tactics.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import {TacticalAdvice} from "../../models/TacticalAdvice";
 
 @Component({
   selector: 'page-tactics',
@@ -8,8 +9,25 @@ import { NavController } from 'ionic-angular';
 })
 export class TacticsPage {
 
-  constructor(public navCtrl: NavController) {
+  tactics: Array<TacticalAdvice>;
 
+  constructor(public navCtrl: NavController) {
+    this.fetchTactics();
+  }
+
+  private fetchTactics = () => {
+    this.tactics = [
+      new TacticalAdvice(
+        'Increase Shots on Target',
+        'Rather than simply increasing the number of shots your team is taking, concentrate on improving the number of shots that hit the target. This could be acheived by practicing shooting to improve accuracy, or by moving the ball into better areas to shoot from.',
+        ['Diagonal Layoff', '1 Touch Combinations', 'Supporting the Player with the Ball']
+      ),
+      new TacticalAdvice(
+        'Decrease Yellow Cards',
+        'Players with early yellow cards must play more carefully for the rest of the match, potentially negatively effecting their performance. Practice more conservative tackling to decrease the chances of picking up yellow cards.',
+        ['1 v 1 Defending the Dribble', 'Shepherding Players Without Tackling']
+      ),
+    ]
   }
 
 }
