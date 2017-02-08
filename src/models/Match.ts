6a99f3f2c9c7986cf0a5f8739aa3a547460de689
Kevin Doyle <C13547287@mydit.ie>
2017-02-08 15:01:16 +0000

    Add remaining match fields to Match class

--- src/models/Match.ts
+++ src/models/Match.ts
@@ -1,9 +1,23 @@
 export class Match {
   homeTeam: string;
   awayTeam: string;
+  date: Date;
   fullTimeHomeGoals: number;
   fullTimeAwayGoals: number;
-  date: Date;
+  halfTimeHomeGoals: number;
+  halfTimeAwayGoals: number;
+  homePossession: number;
+  awayPossession: number;
+  homeTotalShots: number;
+  awayTotalShots: number;
+  homeCorners: number;
+  awayCorners: number;
+  homeFouls: number;
+  awayFouls: number;
+  homeYellowCards: number;
+  awayYellowCards: number;
+  homeRedCards: number;
+  awayRedCards: number;
 
   constructor(homeTeam: string, awayTeam: string, fullTimeHomeScore: number, fullTimeAwayScore: number, date: Date) {
     this.homeTeam = homeTeam;
