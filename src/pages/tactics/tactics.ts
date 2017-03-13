664defc6a2e8e8ae864171d8298e6e12f4862bdd
Kevin Doyle <C13547287@mydit.ie>
2017-03-13 17:22:08 +0000

    Display chart for goals and half time goals

--- src/pages/tactics/tactics.ts
+++ src/pages/tactics/tactics.ts
@@ -1,19 +1,43 @@
-import { Component } from '@angular/core';
+import {Component, ViewChild} from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import {Chart} from 'chart.js';
+
 import {TacticalAdvice} from "../../api/models/TacticalAdvice";
+import {Match} from "../../api/models/Match";
+import {LocalStorage} from "../../providers/local-storage";
 
 @Component({
   selector: 'page-tactics',
-  templateUrl: 'tactics.html'
+  templateUrl: 'tactics.html',
+  providers: [LocalStorage]
 })
 export class TacticsPage {
 
-  tactics: Array<TacticalAdvice>;
+  @ViewChild('goalsCanvas') goalsCanvas;
+  @ViewChild('halfTimeGoalsCanvas') halfTimeGoalsCanvas;
 
-  constructor(public navCtrl: NavController) {
+  goalsChart: any;
+  halfTimeGoalsChart: any;
+
+  tactics: Array<TacticalAdvice>;
+  matches: Array<Match>;
+
+  constructor(public navCtrl: NavController, public localStorage: LocalStorage) {
     this.fetchTactics();
+    this.fetchMatches();
   }
+
+  ionViewDidLoad() {
+    let datasets = this.createChartData(this.matches);
+
+    this.goalsChart = new Chart(this.goalsCanvas.nativeElement, datasets[0]);
+    this.halfTimeGoalsChart = new Chart(this.halfTimeGoalsCanvas.nativeElement, datasets[1]);
+  }
+
+  private fetchMatches = () => {
+    this.matches = this.localStorage.getMatches();
+  };
 
   private fetchTactics = () => {
     this.tactics = [
@@ -28,6 +52,51 @@ export class TacticsPage {
         ['1 v 1 Defending the Dribble', 'Shepherding Players Without Tackling']
       ),
     ]
-  }
+  };
+
+  private createChartData = (matches: Array<Match>) => {
+    let numMatches = matches.length;
+    if (numMatches == 0) {
+      return;
+    }
+
+    // Create the structure to hold the object for each stat
+    let statsDatasets = [];
+    for (let i = 0; i < matches[0].stats.length; i++) {
+      statsDatasets.push({
+        type: 'line',
+        data: {
+          labels: [],
+          datasets: [{
+            data: [],
+            backgroundColor: 'rgba(76, 175, 80, 0.4)'
+          }]
+        },
+        options: {
+          legend: {
+            display: false,
+          },
+          title: {
+            display: true,
+            text: 'Last ' + numMatches + ' Matches'
+          }
+        }
+      });
+    }
+
+    for (let i = 0; i < matches.length; i++) {
+      let match = this.matches[i];
+      let m = Match.convertMatchForBackend(match);
+
+      statsDatasets[0].data.labels.push('');
+      statsDatasets[0].data.datasets[0].data.push(m['full_time_goals']);
+
+      statsDatasets[1].data.labels.push('');
+      statsDatasets[1].data.datasets[0].data.push(m['half_time_goals']);
+
+    }
+
+    return statsDatasets;
+  };
 
 }
